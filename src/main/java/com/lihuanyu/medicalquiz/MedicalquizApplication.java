bfcf34035410453f6f763077fff6ccd1a79dde22
echaoo <1299529160@qq.com>
2016-03-04 11:23:36 +0800

    init

---
+++ src/main/java/com/lihuanyu/medicalquiz/MedicalquizApplication.java
@@ -0,0 +1,12 @@
+package com.lihuanyu.medicalquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalquizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalquizApplication.class, args);
+	}
+}

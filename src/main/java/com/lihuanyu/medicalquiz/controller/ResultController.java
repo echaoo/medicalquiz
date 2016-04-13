579b33263e59c12d811205b6e9b6a1315b4ea1cd
skyADMIN <546485299@qq.com>
2016-04-13 22:38:53 +0800

    add controller to deal test result

---
+++ src/main/java/com/lihuanyu/medicalquiz/controller/ResultController.java
@@ -0,0 +1,16 @@
+package com.lihuanyu.medicalquiz.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by skyADMIN on 16/4/13.
+ */
+@Controller
+public class ResultController {
+
+    @RequestMapping("/posttestresult1")
+    public void dealTestResult1(String answer, String time){
+
+    }
+}

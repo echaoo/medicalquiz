dc83f35115acf9cccd7ac5a4e732a3dc14ce0a8a
skyADMIN <546485299@qq.com>
2016-04-14 20:01:27 +0800

    管理界面临时版

--- src/main/java/com/lihuanyu/medicalquiz/controller/AdminController.java
+++ src/main/java/com/lihuanyu/medicalquiz/controller/AdminController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpSession;
 import java.util.Collection;
 
 /**
@@ -15,6 +16,11 @@ import java.util.Collection;
 @Controller
 public class AdminController {
 
+    @Autowired
+    private TestInfoDao testInfoDao;
+
+    @Autowired
+    private HttpSession httpSession;
 
     @RequestMapping("/admin")
     public String showLogin() {
@@ -26,13 +32,25 @@ public class AdminController {
         if (username.equals("admin") && password.equals("admin")) {
             //Iterable<Participator> participators = participatorDao.findAll();
             //model.addAttribute("adminLists", participators);
-            return "admin";
+            httpSession.setAttribute("adminlogin","true");
+            return "redirect:/adminpanel";
         } else {
             String result = "出错了！";
             String word = "账号或密码有误";
             model.addAttribute("title", result);
             model.addAttribute("word", word);
             return "message";
+        }
+    }
+
+    @RequestMapping("/adminpanel")
+    public String adminPanel(Model model, String action){
+        if (httpSession.getAttribute("adminlogin") == null){
+            return "loginadmin";
+        }else {
+            Iterable<TestInfo> testInfos = testInfoDao.findAll();
+            model.addAttribute("testInfos",testInfos);
+            return "admin";
         }
     }
 

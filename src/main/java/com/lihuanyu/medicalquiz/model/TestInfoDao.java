e9bf3303bf4d3ad8e0db21dad77923dc0e9efb27
skyADMIN <546485299@qq.com>
2016-04-14 20:09:25 +0800

    add two method to get data

--- src/main/java/com/lihuanyu/medicalquiz/model/TestInfoDao.java
+++ src/main/java/com/lihuanyu/medicalquiz/model/TestInfoDao.java
@@ -6,4 +6,6 @@ import org.springframework.data.repository.CrudRepository;
  * Created by skyADMIN on 16/4/14.
  */
 public interface TestInfoDao extends CrudRepository<TestInfo,Integer> {
+    public Iterable<TestInfo> findById(int id);
+    public Iterable<TestInfo> findByUserid(int id);
 }

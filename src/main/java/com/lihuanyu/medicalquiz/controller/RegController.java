b724685633dbb59367f4009b3d12243ab527ab0b
skyADMIN <546485299@qq.com>
2016-04-14 16:46:54 +0800

    更新控制器

--- src/main/java/com/lihuanyu/medicalquiz/controller/RegController.java
+++ src/main/java/com/lihuanyu/medicalquiz/controller/RegController.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpSession;
+
 /**
  * Created by skyADMIN on 16/4/14.
  */
@@ -16,6 +18,9 @@ public class RegController {
 
     @Autowired
     private RegService regService;
+
+    @Autowired
+    private HttpSession httpSession;
 
     @RequestMapping(value = "/savenew",method = RequestMethod.POST)
     public String dealReg(String phone, String name, String sex, String age, String number, String birth, String education, String occupation, String marital, String wherelive, String live, String hypertension, String hypertensiontime, String hypertionsion, String diabetes, String diabetestime, String hyperlipemia, String hyperlipemiatime, String LDLC, String HDLC, String TC, String TG, String angiocardiopathy, String heartpaintime, String myocardial, String AF, String ACVD, String headblood, String chocke, String headchocke, String TIA, String headhurt, String illposition, String illlevel, String drinkstate, String smoke, String isdrinktea, String sleepstate, String PEstate, String socialstate, String hobby, String eathabits){
@@ -27,7 +32,12 @@ public class RegController {
         BasicInfo basicInfo = regService.dealMessage(phone, name, sex, age, number, birth, education, occupation, marital, wherelive, live, hypertension, hypertensiontime, hypertionsion, diabetes, diabetestime, hyperlipemia, hyperlipemiatime, LDLC, HDLC, TC, TG, angiocardiopathy, heartpaintime, myocardial, AF, ACVD, headblood, chocke, headchocke, TIA, headhurt, illposition, illlevel, drinkstate, smoke, isdrinktea, sleepstate, PEstate, socialstate, hobby, eathabits);
 
         regService.saveToDatabase(basicInfo);
+        httpSession.setAttribute("phone",phone);
+        return "datumtest";
+    }
 
-        return "hh";
+    @RequestMapping("/regnew")
+    public String showReg() {
+        return "regpage";
     }
 }
--- src/main/java/com/lihuanyu/medicalquiz/controller/TestController.java
+++ src/main/java/com/lihuanyu/medicalquiz/controller/TestController.java
@@ -1,7 +1,6 @@
 package com.lihuanyu.medicalquiz.controller;
 
-import com.lihuanyu.medicalquiz.model.Datum;
-import com.lihuanyu.medicalquiz.model.DatumDao;
+import com.lihuanyu.medicalquiz.services.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,17 +13,61 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class TestController {
 
     @Autowired
-    private DatumDao datumDao;
-    @RequestMapping(value = "/savetest",method = RequestMethod.GET)
-    public String saveTestFour(int four1,int four2,int four3,int four4){
-        Datum datum = new Datum();
-      int score = four1+four2+four3+four4;
-        datum.setScore(score);
-        datumDao.save(datum);
-        if(score <=2){
-            return "fivetest";
-        }else {
+    private TestService testService;
+
+    @RequestMapping(value = "/savemmse", method = RequestMethod.POST)
+    public String saveMmseTest(String ques1, String ques2, String ques3, String ques4, String ques5, String ques6, String ques7, String ques8, String ques9, String ques10, String ques11, String ques12, String ques13, String ques14, String ques15, String ques16, String ques17, String ques18, String ques19, String ques20) {
+        int ans = addAllQues(ques1, ques2, ques3, ques4, ques5, ques6, ques7, ques8, ques9, ques10, ques11, ques12, ques13, ques14, ques15, ques16, ques17, ques18, ques19, ques20);
+        String detail = ques1 + ques2 + ques3 + ques4 + ques5 + ques6 + ques7 + ques8 + ques9 + ques10 + ques11 + ques12 + ques13 + ques14 + ques15 + ques16 + ques17 + ques18 + ques19 + ques20;
+        testService.dealEmseTest(ans, detail);
+        return "finishtest";
+    }
+
+    @RequestMapping("/test")
+    public String showTest() {
+        return "datumtest";
+    }
+
+    @RequestMapping(value = "/savedatum", method = RequestMethod.POST)
+    public String saveTestFour(String ques1, String ques2, String ques3, String ques4) {
+        int score = Integer.parseInt(ques1) + Integer.parseInt(ques2) + Integer.parseInt(ques3) + Integer.parseInt(ques4);
+        String detail = ques1 + ques2 + ques3 + ques4;
+        if (score <= 2) {
+            testService.dealContinueTest(detail, score);
+            return "mmsetest";
+        } else {
+            testService.dealDatumTest(detail, score);
             return "finishtest";
         }
     }
+
+    private int addAllQues(String ques1, String ques2, String ques3, String ques4, String ques5, String ques6, String ques7, String ques8, String ques9, String ques10, String ques11, String ques12, String ques13, String ques14, String ques15, String ques16, String ques17, String ques18, String ques19, String ques20) {
+        int ans = 0;
+        ans = stringToIntadd(ans, ques1);
+        ans = stringToIntadd(ans, ques2);
+        ans = stringToIntadd(ans, ques3);
+        ans = stringToIntadd(ans, ques4);
+        ans = stringToIntadd(ans, ques5);
+        ans = stringToIntadd(ans, ques6);
+        ans = stringToIntadd(ans, ques7);
+        ans = stringToIntadd(ans, ques8);
+        ans = stringToIntadd(ans, ques9);
+        ans = stringToIntadd(ans, ques10);
+        ans = stringToIntadd(ans, ques11);
+        ans = stringToIntadd(ans, ques12);
+        ans = stringToIntadd(ans, ques13);
+        ans = stringToIntadd(ans, ques14);
+        ans = stringToIntadd(ans, ques15);
+        ans = stringToIntadd(ans, ques16);
+        ans = stringToIntadd(ans, ques17);
+        ans = stringToIntadd(ans, ques18);
+        ans = stringToIntadd(ans, ques19);
+        ans = stringToIntadd(ans, ques20);
+        return ans;
+    }
+
+    public int stringToIntadd(int ans, String m) {
+        return ans + Integer.parseInt(m);
+    }
+
 }

ea5ce36e50785b8d82926ebed827c53b714be070
echaoo <1299529160@qq.com>
2016-04-14 15:42:43 +0800

    第二次测试提交数据正确

--- fontend/app/scripts/emtest.js
+++ fontend/app/scripts/emtest.js
@@ -5,25 +5,24 @@
 var timedown;
 var correctrate1 = "";
 var correctrate2 = "";
-function postresult(){
-  $.post( "test.php", { correct:correctrate1 , time: "2pm" } );
+function postresult(url,correctrate) {
+  $.post(url, {correct: correctrate, time: '2px'});
 }
 
-function endtest1(correctdata1) {
-  postresult(correctdata1);
+function endtest1() {
+  var url1 = '/saveemtest1';
+  var correctrate = correctrate1;
+  postresult(url1, correctrate);
   $('#testcontent').hide();
   $('#over1').show();
 }
 
-function endtest2(correctrate2){
-  postresult(correctrate2);
+function endtest2() {
+  var correctrate = correctrate2;
+  var url2 = '/saveemtest2';
+  postresult(url2, correctrate);
   $('#testcontent').hide();
   $('#over2').show();
-}
-function fillForm(val) {
-  var now = $('#emtestresult1').val();
-  now = now + val;
-  $('#emtestresult1').val(now);
 }
 
 function isChooseTrue(url, choose) {
@@ -31,31 +30,27 @@ function isChooseTrue(url, choose) {
   if (parseInt(arr[2]) <= 20) {
     if (choose == 'in') {
       //right
-      correctrate1+="1";
-      fillForm("1");
+      correctrate1 += "1";
     } else {
       //wrong
-      correctrate1+="2";
-      fillForm("2");
+      correctrate1 += "2";
     }
   } else {
     if (choose == "in") {
       //wrong
-      correctrate1+="2";
-      fillForm("2");
+      correctrate1 += "2";
     } else {
       //right
-      correctrate1+="1";
-      fillForm("1");
+      correctrate1 += "1";
     }
   }
 }
-function isTrue(choose){
-  if (choose=="yes"){
-    correctrate2+=1;
-  }else if(choose =="maybe"){
-    correctrate2+=2;
-  }else correctrate2+=0;
+function isTrue(choose) {
+  if (choose == "yes") {
+    correctrate2 += 1;
+  } else if (choose == "maybe") {
+    correctrate2 += 2;
+  } else correctrate2 += 0;
 }
 function countDownTime(currentTime, picUrl) {
   'use strict';
@@ -141,7 +136,7 @@ $(document).ready(function ($) {
     clearInterval(timedown);
     if (urls[flag] != '0') {
       $('#pictureurl').attr('src', urls[flag]);
-    }else{
+    } else {
       endtest1();
     }
   }
@@ -183,26 +178,23 @@ $(document).ready(function ($) {
     $('#description2').show();
   });
 
-  function recheck(para){
+  function recheck(para) {
     var d2 = new Date();
     time2 = d2.getTime();
     var time = time2 - time1;
     var url = $('#pictureurl').attr('src');
-    //isTrue(para);
+    isTrue(para);
     clearInterval(timedown);
     if (urls[flag] != '0') {
       $('#pictureurl').attr('src', urls[flag]);
-    }else{
+    } else {
       endtest2();
     }
   }
 
 //第二次开始测试
-  $('#teststart2').click(function(){
+  $('#teststart2').click(function () {
     flag = 0;
-    //urls.sort(function () {
-    //  return 0.5 - Math.random();
-    //});//打乱顺序
     $('#description2').hide();
     $('#testcontent').show();
     $('#in').hide();

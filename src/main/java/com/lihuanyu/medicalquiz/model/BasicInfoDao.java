4a7f1ea9d045ff72165974dd62fdec5ba9584513
skyADMIN <546485299@qq.com>
2016-04-14 16:47:17 +0800

    添加通过手机号查找基础信息的方法

--- src/main/java/com/lihuanyu/medicalquiz/model/BasicInfoDao.java
+++ src/main/java/com/lihuanyu/medicalquiz/model/BasicInfoDao.java
@@ -6,5 +6,5 @@ import org.springframework.data.repository.CrudRepository;
  * Created by echao on 2016/3/8.
  */
 public interface BasicInfoDao extends CrudRepository<BasicInfo,Integer> {
-
+    public BasicInfo findByPhonenumber(String phonenumber);
 }
